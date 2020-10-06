dd9bf8e5a7dc6dced9740de26b1d052ba7f70698
olliesmith3 <olliesmith3@rocketmail.com>
2020-10-06 16:14:08 +0100

    added up, down, switchPowerSaving, reset methods with tests

--- jasmine/jasmine-standalone-3.6.0/spec/thermostatSpec.js
+++ jasmine/jasmine-standalone-3.6.0/spec/thermostatSpec.js
@@ -6,4 +6,48 @@ describe("Thermostat", function() {
       var thermostat = new Thermostat();
       expect(thermostat.temperature).toEqual(20)
     });
+
+    it('temperature can be increased', function() {
+      var thermostat = new Thermostat();
+      thermostat.up(5);
+      expect(thermostat.temperature).toEqual(25)
+    });
+
+    it('temperature can be decreased', function() {
+      var thermostat = new Thermostat();
+      thermostat.down(4);
+      expect(thermostat.temperature).toEqual(16)
+    });
+
+    it('minimum temperature is 10 degrees', function() {
+      var thermostat = new Thermostat();
+      thermostat.down(15);
+      expect(thermostat.temperature).toEqual(10)
+    });
+
+    it('switchPowerSaving changes the power saving mode', function() {
+      var thermostat = new Thermostat();
+      thermostat.switchPowerSaving();
+      expect(thermostat.powerSaving).toBe(false)
+    });
+
+    it('max temperature is 25 degrees when power saving mode on', function() {
+      var thermostat = new Thermostat();
+      thermostat.up(15);
+      expect(thermostat.temperature).toEqual(25)
+    });
+
+    it('max temperature is 32 degrees when power saving mode off', function() {
+      var thermostat = new Thermostat();
+      thermostat.switchPowerSaving();
+      thermostat.up(15);
+      expect(thermostat.temperature).toEqual(32)
+    });
+
+    it('the reset function returns the temperature to 20', function() {
+      var thermostat = new Thermostat();
+      thermostat.up(15);
+      thermostat.reset();
+      expect(thermostat.temperature).toEqual(20)
+    });
 });
--- jasmine/jasmine-standalone-3.6.0/src/thermostat.js
+++ jasmine/jasmine-standalone-3.6.0/src/thermostat.js
@@ -2,6 +2,33 @@ class Thermostat {
 
   constructor() {
     this.temperature = 20;
+    this.powerSaving = true
+  }
+
+  up(degrees) {
+    if (this.powerSaving === true && this.temperature + degrees > 25) {
+      this.temperature = 25
+    } else if (this.powerSaving === false && this.temperature + degrees > 32) {
+      this.temperature = 32
+    } else {
+      this.temperature += degrees
+    }
+  }
+
+  down(degrees) {
+    if (this.temperature - degrees < 10) {
+      this.temperature = 10
+    } else {
+      this.temperature -= degrees
+    }
+  }
+
+  switchPowerSaving() {
+    this.powerSaving = !this.powerSaving
+  }
+
+  reset() {
+    this.temperature = 20
   }
 
 }
